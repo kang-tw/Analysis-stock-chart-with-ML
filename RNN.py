135c71aadb68456baa67624984cacd457943b880
kang-tw <137156706+kang-tw@users.noreply.github.com>
2023-11-24 22:29:04 +0900

    test data

--- RNN.py
+++ RNN.py
@@ -1,27 +1,31 @@
 import tensorflow as tf
 from keras import Sequential
 from keras.layers import Dense, LSTM, Dropout
+from make_namalize import train_data,train_result,test_data,test_result
+from keras.optimizers import Adam
 
 
 model = Sequential()
+model.add(LSTM(units=10, activation='relu', input_shape=(5, 4), return_sequences=True))
+model.add(Dropout(0.2)) # 과적합 방지 랜덤한 10프로의 모듈 비활성화
 
-model.add(LSTM(units=20, activation='relu', input_shape=(10, 4)))
-model.add(Dropout(0.1)) # 과적합 방지 랜덤한 10프로의 모듈 비활성화
-
-model.add(LSTM(units=20, activation='relu'))
-model.add(Dropout(0.1))
+model.add(LSTM(units=10, activation='relu'))
+model.add(Dropout(0.2))
 
 
+optimizer = Adam(learning_rate=0.1)
 model.add(Dense(units=1)) # fully connected layer 최종적인 모델의 결과값을 도출하기 위해 사용 
-model.summary()
+#model.summary()
+model.compile(optimizer=optimizer, loss='mean_squared_error',metrics='accuracy' )
 
 
- 
 
-
-model.compile(optimizer='adam', loss='mean_squared_error')
 with tf.device("/device:GPU:0"): # GPU 설정 명령어 gpu를 인식할 경우 default로 잡을 수 있긴하다. 
-    model.fit(전처리 데이터 값, 전처리 결과값, epochs=70, batch_size=30)
-model.save('model_name.h5') # 학습된 데이터중에 잘 학습된 데이터가 존재한다면 모델 키핑
+    model.fit(train_data, train_result, epochs=70, batch_size=1)
 
-pred_y = model.predict(test_X) # 예측된 y값 
+
+
+
+#pred_y = model.predict(test_data)
+
+#model.save('model_name.h5') # 학습된 데이터중에 잘 학습된 데이터가 존재한다면 모델 키핑
