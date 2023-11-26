9cc4e8e668b5be808b051c573e7e42700b1052da
kang-tw <137156706+kang-tw@users.noreply.github.com>
2023-11-26 20:19:33 +0900

    add coin

--- data_processing_with_coin.py
+++ data_processing_with_coin.py
@@ -9,19 +9,17 @@ real_result = real_result.iloc[1:, :]
 def min_max_Scaler(data):
     return (data - np.min(data, 0) )/ (np.max(data, 0) - np.min(data, 0) + 1e-15)
 
-
 csv_data = min_max_Scaler(csv_data)
 real_result=min_max_Scaler(real_result)
 
 csv_data = csv_data.values.tolist()
 real_result = real_result.values.tolist()
 
-
-
 data = []
 data_result = []
 
-size=5
+size= 20
+result_size = size
 
 for i in range(len(real_result) - size): # 5일간의 데이터로 다음날의 주가 예측 
     temp_data = csv_data[i : i + size] 
@@ -31,21 +29,16 @@ for i in range(len(real_result) - size): # 5일간의 데이터로 다음날의 
 
 
 
-
-
-
 train_size = int(len(data_result) * 0.7)
 train_data = np.array(data[0 : train_size])
 train_result = np.array(data_result[0 : train_size])
 
 
-test_size = len(data_result) - train_size
-test_data = np.array(data[train_size : len(data)])
-test_result = np.array(data_result[train_size : len(data_result)])
+#test_size = len(data_result) - train_size
+test_data = np.array(data[train_size : ])
+test_result = np.array(data_result[train_size : ])
 
 
 
-print(train_data.shape)
-print(train_result.shape)
-train_data =train_data[:-1]
-train_result =train_result[:-1]
+# print(train_data.shape)
+# print(train_result.shape)
