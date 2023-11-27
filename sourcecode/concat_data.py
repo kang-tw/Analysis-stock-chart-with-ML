78c095ef9c98563a79bdfb9fc2075f9e1222f22d
Bang-hy <ghdud4642@naver.com>
2023-11-27 22:02:42 +0900

    get mid result

--- sourcecode/concat_data.py
+++ sourcecode/concat_data.py
@@ -18,4 +18,10 @@ df = yf.download('005930.KS','2023-01-02','2023-06-30')
 df.to_excel("삼성전자.xlsx")
 df.to_csv("삼성전자.csv")
 
+from datetime import datetime
+converted_keys = [datetime.strptime(key, '%Y-%m-%d %H:%M:%S') for key in data.keys()]
+df_w = pd.DataFrame(list(zip(converted_keys, data.values())), columns=['date', 'weight'])
+df_w.set_index('date', inplace=True)
+
 result_df = pd.concat([df, df_w], axis=1)
+result_df.to_csv('fin_output.csv', encoding='utf-8')
