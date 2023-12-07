3242156a00d111fb1307cbcf80ce7c4609380c94
unknown <ykk13922@naver.com>
2023-12-07 22:02:51 +0900

    updata real time automated

--- ML/model_RNN_good_with_coin/RNN_with_coin.py
+++ ML/model_RNN_good_with_coin/RNN_with_coin.py
@@ -3,6 +3,8 @@ from keras import Sequential
 from keras.layers import Dense, LSTM, Dropout,Flatten
 from data_processing_with_coin import train_data,train_result,test_result,test_data
 from keras.optimizers import Adam
+import time
+import datetime
 
 
 
@@ -26,26 +28,34 @@ hit =0
 count = 0
 model = make_model()
 
-
 while(True):
-    count =count+1
-    with tf.device("/device:GPU:0"): # GPU 설정 명령어 gpu를 인식할 경우 default로 잡을 수 있긴하다. 
-        model.fit(train_data, train_result, epochs=10, batch_size=200)
-    pred_y = model.predict(test_data)
-    avg=0
-    for i,j in zip(pred_y,test_result):
-        avg += abs(1 -j/i)
+    while(True):
+        count =count+1
+        with tf.device("/device:GPU:0"): # GPU 설정 명령어 gpu를 인식할 경우 default로 잡을 수 있긴하다. 
+            model.fit(train_data, train_result, epochs=10, batch_size=200)
+        pred_y = model.predict(test_data)
+        avg=0
+        for i,j in zip(pred_y,test_result):
+            avg += abs(1 -j/i)
+            
+        avg= avg/len(pred_y) * 100
+        print(avg)
         
-    avg= avg/len(pred_y) * 100
-    print(avg)
-    
-    if(avg<0.9):hit=hit+1
-    if(hit == 2): break
-    if(count == 30):
-         model=make_model()
-         count = 0
-    
+        if(avg<0.9):hit=hit+1
+        if(hit == 2): break
+        if(count == 30):
+            model=make_model()
+            count = 0
+        
 
 
-model.save('model_RNN.h5') # 학습된 데이터중에 잘 학습된 데이터가 존재한다면 모델 킵
+    model.save('model_RNN.h5') # 학습된 데이터중에 잘 학습된 데이터가 존재한다면 모델 킵
+    current = datetime.datetime.now()
+    criterion = current.replace(hour=15, minute=30, second=0, microsecond=0)
+
+    criterion += datetime.timedelta(days=1) # 다음날 15시 30분 겨냥 
+
+    sleep_time = (criterion - current).total_seconds()
+    print(sleep_time)
+    time.sleep(sleep_time)
 
--- ML/stock/RNN.py
+++ ML/stock/RNN.py
@@ -6,6 +6,8 @@ from keras.optimizers import Adam
 import matplotlib.pyplot as plt
 import pandas as pd
 import numpy as np
+import time
+import datetime
 
 path = './fin_output.csv'
 csv_data = pd.read_csv(path)
@@ -31,44 +33,55 @@ hit =0
 count = 0
 model = make_model()
 
-
 while(True):
-    count =count+1
-    with tf.device("/device:GPU:0"): # GPU 설정 명령어 gpu를 인식할 경우 default로 잡을 수 있긴하다. 
-        model.fit(train_data, train_result, epochs=10, batch_size=10)
-    pred_y = model.predict(test_data)
-    avg=0
-    for i,j in zip(pred_y,test_result):
-        avg += abs(1 -j/i)
+
+    while(True):
+        count =count+1
+        with tf.device("/device:GPU:0"): # GPU 설정 명령어 gpu를 인식할 경우 default로 잡을 수 있긴하다. 
+            model.fit(train_data, train_result, epochs=10, batch_size=10)
+        pred_y = model.predict(test_data)
+        avg=0
+        for i,j in zip(pred_y,test_result):
+            avg += abs(1 -j/i)
+            
+        avg= avg/len(pred_y) * 100
+        print(avg)
         
-    avg= avg/len(pred_y) * 100
-    print(avg)
-    
-    if(avg<8.3):hit=hit+1
-    if(hit == 2): break
-    if(count == 30):
-         model=make_model()
-         count = 0
-    
+        if(avg<8.3):hit=hit+1
+        if(hit == 2): break
+        if(count == 30):
+            model=make_model()
+            count = 0
+        
 
-model.save('model_RNN.h5') # 학습된 데이터중에 잘 학습된 데이터가 존재한다면 모델 키핑
+    model.save('model_RNN.h5') # 학습된 데이터중에 잘 학습된 데이터가 존재한다면 모델 키핑
+    current = datetime.datetime.now()
+    criterion = current.replace(hour=15, minute=30, second=0, microsecond=0)
+
+    criterion += datetime.timedelta(days=1) # 다음날 15시 30분 겨냥 
+
+    sleep_time = (criterion - current).total_seconds()
+    print(sleep_time)
+    time.sleep(sleep_time)
 
 
-plt.figure()
-plt.plot(test_result, color='red', label='real price')
-plt.plot(pred_y, color='blue', label='predicted price')
-plt.title('price prediction')
 
-x_values = np.arange(len(pred_y))
 
-plt.xlabel('time')
-plt.ylabel('price')
-plt.xticks(x_values)
+# plt.figure()
+# plt.plot(test_result, color='red', label='real price')
+# plt.plot(pred_y, color='blue', label='predicted price')
+# plt.title('price prediction')
 
-plt.legend()
-plt.grid()
-plt.show()
-np.savetxt('samsung_output.csv', test_result, delimiter=',')
-np.savetxt('samsung_pred_output.csv', pred_y, delimiter=',')
+# x_values = np.arange(len(pred_y))
 
-print("내일 삼성전자 주가 :", csv_data['Close'].iloc[-1] * pred_y[-1] /data_result[-1], 'KRW')
+# plt.xlabel('time')
+# plt.ylabel('price')
+# plt.xticks(x_values)
+
+# plt.legend()
+# plt.grid()
+# plt.show()
+# np.savetxt('samsung_output.csv', test_result, delimiter=',')
+# np.savetxt('samsung_pred_output.csv', pred_y, delimiter=',')
+
+# print("내일 삼성전자 주가 :", csv_data['Close'].iloc[-1] * pred_y[-1] /data_result[-1], 'KRW')
