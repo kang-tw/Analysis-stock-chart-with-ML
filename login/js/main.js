52bb73817f921b5d9897ced05e5bff7e88c18f77
sangwuny <144337247+sangwuny@users.noreply.github.com>
2023-12-14 18:23:41 +0900

    from login

---
+++ login/js/main.js
@@ -0,0 +1,160 @@
+
+(function ($) {
+    "use strict";
+
+
+    /*==================================================================
+    [ Focus input ]*/
+    $('.input100').each(function () {
+        $(this).on('blur', function () {
+            if ($(this).val().trim() != "") {
+                $(this).addClass('has-val');
+            }
+            else {
+                $(this).removeClass('has-val');
+            }
+        })
+    })
+
+
+    /*==================================================================
+    [ Validate ]*/
+    var input = $('.validate-input .input100');
+
+    $('.validate-form').on('submit', function () {
+        var check = true;
+
+        for (var i = 0; i < input.length; i++) {
+            if (validate(input[i]) == false) {
+                showValidate(input[i]);
+                check = false;
+            }
+        }
+
+        return check;
+    });
+
+
+    $('.validate-form .input100').each(function () {
+        $(this).focus(function () {
+            hideValidate(this);
+        });
+    });
+
+    function validate(input) {
+        if ($(input).attr('type') == 'email' || $(input).attr('name') == 'email') {
+            if ($(input).val().trim().match(/^([a-zA-Z0-9_\-\.]+)@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.)|(([a-zA-Z0-9\-]+\.)+))([a-zA-Z]{1,5}|[0-9]{1,3})(\]?)$/) == null) {
+                return false;
+            }
+        }
+        else {
+            if ($(input).val().trim() == '') {
+                return false;
+            }
+        }
+    }
+
+    function showValidate(input) {
+        var thisAlert = $(input).parent();
+
+        $(thisAlert).addClass('alert-validate');
+    }
+
+    function hideValidate(input) {
+        var thisAlert = $(input).parent();
+
+        $(thisAlert).removeClass('alert-validate');
+    }
+
+    /*==================================================================
+    [ Show pass ]*/
+    var showPass = 0;
+    $('.btn-show-pass').on('click', function () {
+        if (showPass == 0) {
+            $(this).next('input').attr('type', 'text');
+            $(this).find('i').removeClass('zmdi-eye');
+            $(this).find('i').addClass('zmdi-eye-off');
+            showPass = 1;
+        }
+        else {
+            $(this).next('input').attr('type', 'password');
+            $(this).find('i').addClass('zmdi-eye');
+            $(this).find('i').removeClass('zmdi-eye-off');
+            showPass = 0;
+        }
+
+    });
+
+
+})(jQuery);
+
+function login() {
+    const usernameInput = document.getElementById('username');
+    const passwordInput = document.getElementById('password');
+    const username = usernameInput.value;
+    const password = passwordInput.value;
+
+    // 사용자 인증 확인
+    const authenticatedUser = registeredUsers.find(user => user.username === username && user.password === password);
+
+    if (authenticatedUser) {
+        alert('로그인 성공!');
+        // 로그인에 성공하면 여기에서 추가 작업 수행
+        login_success();
+    } else {
+        alert('잘못된 사용자명 또는 비밀번호');
+    }
+
+    // 폼 초기화
+    usernameInput.value = '';
+    passwordInput.value = '';
+}
+
+function login_success() {
+    window.location.href = 'login_success.html';
+}
+
+function sign_up() {
+    alert('회원가입을 진행합니다');
+    window.location.href = 'sign_up.html';
+}
+
+function logout() {
+    alert('로그아웃을 진행합니다');
+    window.location.href = 'index.html';
+}
+
+let registeredUsers = JSON.parse(localStorage.getItem('registeredUsers')) || 
+[
+{ username: 'user1@naver.com', password: 'pass1' },
+{ username: 'user2@naver.com', password: 'pass2' },
+];//DB
+
+function addUser() {
+
+    const usernameInput = document.getElementById('username');
+    const passwordInput = document.getElementById('password');
+    const username = usernameInput.value;
+    const password = passwordInput.value;
+
+    // 이미 등록된 사용자인지 확인
+    const userExists = registeredUsers.some(user => user.username === username || user.password === password);
+
+    if (userExists) {
+        alert('이미 등록된 사용자입니다.');
+    } else {
+        // 새로운 사용자 정보를 추가
+        registeredUsers.push({ username, password });
+        localStorage.setItem('registeredUsers', JSON.stringify(registeredUsers));
+        alert('사용자가 성공적으로 추가되었습니다.');
+        sign_up_success();
+    }
+
+    // 폼 초기화
+    usernameInput.value = '';
+    passwordInput.value = '';
+}
+
+function sign_up_success() {
+    window.location.href = 'index.html';
+}
